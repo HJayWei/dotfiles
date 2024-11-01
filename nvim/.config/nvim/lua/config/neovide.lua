28bfb22a081c2217491a43a34343f6b59573ae41
JWei <jay0935199@gmail.com>
2024-11-01 10:12:33 +0800

    Modify: Set Neovide background transparency

--- nvim/.config/nvim/lua/config/neovide.lua
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -6,6 +6,8 @@ if vim.g.neovide then
   vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
   vim.keymap.set("t", "<D-v>", '<C-\\><C-N>"+pa') -- Paste terminal mode
 
+  vim.g.transparency = 0.9
+  vim.g.neovide_transparency = 0.9
   vim.g.neovide_input_macos_option_key_is_meta = "both"
   vim.g.neovide_cursor_vfx_mode = "ripple"
 
